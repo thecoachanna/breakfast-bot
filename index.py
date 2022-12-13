b16503b28ed14ae308aa076aab0a32b8a49969bd
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2022-12-13 12:39:44 -0500

    handling input

--- index.py
+++ index.py
@@ -1,15 +1,19 @@
 # BREAKFAST BOT GAME
 
-# Get input and use it to determine what happens next
-# Handle bad input without crashing
-# Be flexible with the input the user can enter
+
 # Print messages to the console, with a short pause after each one
 # Allow the player to order again if they like
 
-users_choice = input("Please place your order. Would you like waffles or pancakes?\n")
-if users_choice.lower() == 'waffles':
-    print("Waffles it is!")
-elif users_choice.lower() == 'pancakes':
-    print("Pancakes it is!")
-else:
-    print("Sorry, we don't offer that menu item.")
+while True:
+    # Get input and use it to determine what happens next
+    users_choice = input("Please place your order. Would you like waffles or pancakes?\n").lower()
+    # Be flexible with the input the user can enter
+    if 'waffles' in users_choice:
+        print("Waffles it is!")
+        break
+    elif 'pancakes' in users_choice:
+        print("Pancakes it is!")
+        break
+    # Handle bad input without crashing
+    else:
+        print("Sorry, we don't offer that menu item.")
