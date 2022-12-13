d868092722b046e245f874bd5bce5dbc6f88aaf3
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2022-12-13 12:57:52 -0500

    allow user to order again

--- index.py
+++ index.py
@@ -1,19 +1,48 @@
+import time
+
 # BREAKFAST BOT GAME
 
 
 # Print messages to the console, with a short pause after each one
-# Allow the player to order again if they like
+print("Hello! I am Bob, the Breakfast Bot.")
+time.sleep(2)
+print("Today we have two breakfasts available:")
+time.sleep(2)
+print("The first is waffles with strawberries and whipped cream.")
+time.sleep(2)
+print("The second is sweet potato pancakes with butter and syrup.")
+time.sleep(2)
 
 while True:
-    # Get input and use it to determine what happens next
-    users_choice = input("Please place your order. Would you like waffles or pancakes?\n").lower()
-    # Be flexible with the input the user can enter
-    if 'waffles' in users_choice:
-        print("Waffles it is!")
-        break
-    elif 'pancakes' in users_choice:
-        print("Pancakes it is!")
-        break
-    # Handle bad input without crashing
-    else:
-        print("Sorry, we don't offer that menu item.")
+    while True:
+        # Get input and use it to determine what happens next
+        users_choice = input("Please place your order. Would you like waffles or pancakes?\n").lower()
+        # Be flexible with the input the user can enter
+        if 'waffles' in users_choice:
+            print("Waffles it is!")
+            time.sleep(2)
+            break
+        elif 'pancakes' in users_choice:
+            print("Pancakes it is!")
+            time.sleep(2)
+            break
+        # Handle bad input without crashing
+        else:
+            print("Sorry, we don't offer that menu item.")
+            time.sleep(2)
+    print("Your order will be ready shortly!")
+    time.sleep(2)
+    # Allow the player to order again if they like
+    another_order = input("Would you like to order something else? 'yes' or 'no'?\n").lower()
+    while True:
+        if 'no' in another_order:
+            print("Okay, goodbye!")
+            time.sleep(2)
+            break
+        elif 'yes' in another_order:
+            print("Okay, I'm happy to take another order.")
+            time.sleep(2)
+        else:
+            print("I'm sorry, I don't understand.")
+    if 'no' in another_order:
+        break        
